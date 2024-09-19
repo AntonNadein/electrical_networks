3a60ce2ffb071e03de8b426d430c014685dce665
Anton <fynjy-666@mail.ru>
2024-09-19 16:49:29 +0300

    add class team_dispatcher, tests

--- tests/test_team_dispatcher.py
+++ tests/test_team_dispatcher.py
@@ -1,7 +1,7 @@
 from src.team_dispatcher import Dispatcher, SupervisorDispatcher
 
 
-def test_supervisor_control(test_employee_data_1, test_metering_device):
+def test_supervisor_dispatcher(test_employee_data_1, test_metering_device):
     supervisor = SupervisorDispatcher(
         "Ivan", "Ivanov", "Ivanovich", 40, 20000, [test_employee_data_1], [test_metering_device]
     )
@@ -10,7 +10,7 @@ def test_supervisor_control(test_employee_data_1, test_metering_device):
     assert str(supervisor) == "Ivanov Ivan Ivanovich Возраст:40 Зарплата: 30000.0руб."
 
 
-def test_inspector(test_employee_data_1, test_metering_device):
+def test_dispatcher(test_employee_data_1, test_metering_device):
     dispatcher = Dispatcher("Ivan", "Ivanov", "Ivanovich", 40, 20000, [test_metering_device])
     assert dispatcher.number_of_equipment == 1
     assert str(dispatcher) == "Ivanov Ivan Ivanovich Возраст:40 Зарплата: 26000.0руб."
